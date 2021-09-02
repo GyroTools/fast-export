aee817ec9a49046a30cf8b62aa9b508652de2bd2
Martin <martin.buehrer@gyrotools.com>
2021-09-02 10:17:08 +0200

    fix in test data removal plugin

--- plugins/remove_test_data/__init__.py
+++ plugins/remove_test_data/__init__.py
@@ -8,6 +8,6 @@ class Filter():
     def file_data_filter(self,file_data):
         file_ctx = file_data['file_ctx']
         filename = file_data['filename']
-        if 'tests/data' in str(filename):
+        if 'tests/data/' in str(filename):
             file_data['file_ctx'] = None
             file_data['filename'] = None
